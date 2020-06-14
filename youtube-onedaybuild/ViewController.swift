42627401fc52b5e2b8eadd6d926ee8d646af27a8
Kok Sing <koksing.ooi@hotmail.com>
2020-06-14 15:05:08 +0800

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Ooi Kok Sing on 14/6/20.
+//  Copyright © 2020 ECM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
